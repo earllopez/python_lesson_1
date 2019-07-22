8d07f5e45053b0987ae2ab69e08278d1a984ef87
Earl Lopez <15304537+earllopez@users.noreply.github.com>
2019-07-21 21:00:46 -0400

    Add files via upload

---
+++ Unit 1 - Python Basics.py
@@ -0,0 +1,54 @@
+
+# ## Activity: Expressions
+# 
+# ### Instructions
+# 1. In the new cell below, write the following statements:
+#     - x is equal to the number 25
+#     - y is equal to the number 2
+#     - z is equal to the number 999
+# 2. Write 5 different expressions using x,y and z that evaluate to the number zero(0)
+# 3. For only the 5th expression, assign that expression to a variable using an assignment statement
+#     - Ex. five = (z / y / x) - 7 * y
+#Basically make everything to zero using x, y, z
+
+# In[7]:
+
+
+# First Attempt
+x = 25
+y = 2
+z = 999
+
+((x + y) / (x + y)) - 1
+
+
+# In[49]:
+
+
+# Second Attempt
+((((2**3) * y) + x + z) / 1040) - 1
+
+
+# In[76]:
+
+
+# Third Attempt
+(((z*y) - x) + 20)%2 - 1
+
+
+# In[84]:
+
+
+# Fourth Attempt
+(z / x) / 2*0.04 - 0.0016
+
+
+# In[110]:
+
+
+#Fifth Attempt 
+x = 25
+y = 2
+z = 999
+one_hundred = x + y + z
+(((one_hundred + x * z / z) - (z+(6*8)-4))/2*4)/16-1
